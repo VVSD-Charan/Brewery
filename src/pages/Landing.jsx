943f8eb86820190d9fd341fe155e31ef041462ef
VVSD Charan <vvsdcharan2001@gmail.com>
2023-06-21 15:21:30 +0530

    modified:   src/components/SearchForm.jsx 	modified:   src/pages/Landing.jsx 	modified:   src/pages/Newsletter.jsx

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,13 +1,16 @@
 import { useLoaderData } from 'react-router-dom';
 import axios from 'axios';
 import CocktailList from '../components/CocktailList';
+import SearchForm from '../components/SearchForm';
 
 const cocktailSearchUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
-export const loader = async () =>
+export const loader = async ({request}) =>
 {
-    const searchTerm = '';
+    const url=new URL(request.url);
+
+    const searchTerm = url.searchParams.get('search')||'';
     const response=await axios.get(`${cocktailSearchUrl}${searchTerm}`);
 
     return {drinks:response.data.drinks,searchTerm};
@@ -19,6 +22,7 @@ const Landing = () =>
    
     return (
         <>
+            <SearchForm searchTerm={searchTerm}/>
             <CocktailList drinks={drinks}/>
         </>
     );
--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,4 +1,4 @@
-import { Form,redirect } from 'react-router-dom';
+import { Form,redirect,useNavigation } from 'react-router-dom';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
@@ -9,25 +9,36 @@ export const action = async ({request}) =>
     const formData = await request.formData();
     const data=Object.fromEntries(formData);
 
-    const response = await axios.post(newsletterUrl,data);
-    toast.success(response.data.msg);
-
-    return redirect('/');
+    try
+    {
+        const response = await axios.post(newsletterUrl,data);
+        toast.success(response.data.msg);
+    
+        return redirect('/');
+    }
+    catch(err)
+    {
+        toast.error(err?.response?.data?.msg);
+        return err;
+    }
 }
 
 const Newsletter = () =>
 {
+    const navigation=useNavigation();
+    const isSubmitting = navigation.state==='submitting';
+
     return (
         <Form className="form" method="POST">
             <h4 style={{textAlign:'center',marginBottom:'2rem'}}>Our Newsletter</h4>
             <div className="form-row">
                 <label htmlFor="name" className="form-label">name</label>
-                <input type="text" required className="form-input" name="name" id="name" defaultValue="vvsd"/>
+                <input type="text" required className="form-input" name="name" id="name" />
             </div>
 
             <div className="form-row">
                 <label htmlFor="lastName" className="form-label">last name</label>
-                <input type="text" className="form-input" name="lastName" id="lastName" defaultValue="charan"/>
+                <input type="text" required className="form-input" name="lastName" id="lastName"/>
             </div>
 
             <div className="form-row">
@@ -35,8 +46,8 @@ const Newsletter = () =>
                 <input type="email" className="form-input" name="email" id="email" defaultValue="test@test.com" required/>
             </div>
 
-            <button className="btn btn-block" type="submit" style={{marginTop:'0.5rem',padding:'10px 0'}}>
-                Submit
+            <button className="btn btn-block" type="submit" style={{marginTop:'0.5rem',padding:'10px 0'}} disabled={isSubmitting}>
+                {isSubmitting? 'Submitting ...':'Submit'}
             </button>
         </Form>
     );
